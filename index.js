93b65221def4beb3c9d6f1968de16944dc096f0c
vishalicious213 <vishalicious213@gmail.com>
2023-06-09 14:41:52 -0700

    took control of body in js. drafted renderBackgroundImage()

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Dashboard")
+const body = document.getElementById("body")
+
+// ⬇️ RENDER APP ⬇️
+
+async function renderBackgroundImage() {
+    const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const jsonData = await response.json()
+    body.style.backgroundImage = `url(${jsonData.urls.regular})`
+}
+
+renderBackgroundImage()
