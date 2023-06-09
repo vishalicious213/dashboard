778ed1b0efd6d9c43e16a3bf93090e3c13007026
vishalicious213 <vishalicious213@gmail.com>
2023-06-09 14:57:20 -0700

    drafted renderArtistName(). styled body text

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const body = document.getElementById("body")
+const artistName = document.getElementById("artist-name")
 
 // ⬇️ RENDER APP ⬇️
 
 async function renderBackgroundImage() {
     const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     const jsonData = await response.json()
-    body.style.backgroundImage = `url(${jsonData.urls.regular})`
+    document.body.style.backgroundImage = `url(${jsonData.urls.regular})`
+
+    renderArtistName(jsonData.user.name)
+}
+
+async function renderArtistName(name) {
+    document.getElementById("artist-name").textContent = `By: ${name}`
 }
 
 renderBackgroundImage()
