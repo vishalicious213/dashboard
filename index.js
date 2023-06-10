a79e58a9e635231d3509e1833af9464fda3b4341
vishalicious213 <vishalicious213@gmail.com>
2023-06-09 17:35:32 -0700

    added .catch() to renderBackgroundImage() so that it calls renderDefaultBackgroundImage() if fetch fails

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const artistName = document.getElementById("artist-name")
-
 // ⬇️ RENDER APP ⬇️
 
 async function renderBackgroundImage() {
@@ -14,4 +12,9 @@ async function renderArtistName(name) {
     document.getElementById("artist-name").textContent = `By: ${name}`
 }
 
-renderBackgroundImage()
+function renderDefaultBackgroundImage() {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1535961652354-923cb08225a7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODYzNTU3NDh8&ixlib=rb-4.0.3&q=80&w=1080)`
+    document.getElementById("artist-name").textContent = `By: Simon Berger`
+}
+
+renderBackgroundImage().catch(renderDefaultBackgroundImage)
