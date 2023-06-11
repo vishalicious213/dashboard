b3b9ca7d71199ce22cee8986a7d729951e6388fc
vishalicious213 <vishalicious213@gmail.com>
2023-06-11 12:50:13 -0700

    removed getLocation(). expanded getWeather to incorporate location and get weather info using try/catch and async/await

--- index.js
+++ index.js
@@ -25,14 +25,26 @@ function getCurrentTime() {
     renderTime(time.toLocaleTimeString("en-us", {timeStyle: "medium"}))
 }
 
-function getLocation() {
-    navigator.geolocation.getCurrentPosition(position => {
-        console.log(position)
-    })
-}
-
 async function getWeather() {
-    getLocation()
+    try {
+        const position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+
+        const response = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial`);
+
+        if (!response.ok) {
+            throw new Error("Weather data not available");
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 // ⬇️ RENDER APP ⬇️
